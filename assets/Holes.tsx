89ace91b6bcab9892173b95895603b2ec246a5a9
= <=>
2019-02-07 14:55:58 +0100

    Added assets folder, added some assets.

---
+++ assets/Holes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Holes" tilewidth="300" tileheight="300" tilecount="9" columns="3">
+ <image source="holes-300dpi.png" width="900" height="900"/>
+</tileset>
---
+++ assets/RoboRallyTest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="RoboRallyTest" tilewidth="300" tileheight="300" tilecount="91" columns="7">
+ <image source="standard-300dpi.png" width="2100" height="3900"/>
+</tileset>
