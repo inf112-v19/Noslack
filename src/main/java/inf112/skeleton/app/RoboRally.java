34fb01b242093cf8ee4bac14fce1cd16518dd2f2
= <=>
2019-02-12 15:42:34 +0100

    Added getOrientation() in Player and GameObjectsTests.

--- src/main/java/inf112/skeleton/app/RoboRally.java
+++ src/main/java/inf112/skeleton/app/RoboRally.java
@@ -3,6 +3,7 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -13,7 +14,7 @@ import inf112.skeleton.app.gameobjects.Player;
 
 import java.util.PriorityQueue;
 
-public class RoboRally extends Game {
+public class RoboRally extends Game implements InputProcessor {
     //private BitmapFont font;
 
     // Grid and tile specifications
@@ -185,5 +186,45 @@ public class RoboRally extends Game {
     @Override
     public void dispose() {
         batch.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
