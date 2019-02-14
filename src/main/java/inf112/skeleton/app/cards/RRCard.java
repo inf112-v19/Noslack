1ecf4a6bc42134828aea32cf0b5287a58f453b32
Carl Ravndal <carl.ravndal@gmail.com>
2019-02-14 14:38:15 +0100

    Made RRCard comparable

--- src/main/java/inf112/skeleton/app/cards/RRCard.java
+++ src/main/java/inf112/skeleton/app/cards/RRCard.java
@@ -2,8 +2,7 @@ package inf112.skeleton.app.cards;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public interface RRCard {
-    void  evaluateSprite();
+public interface RRCard extends Comparable{
 
     Sprite getSprite();
 
