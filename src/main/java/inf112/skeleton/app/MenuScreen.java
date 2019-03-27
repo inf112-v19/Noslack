be42a71f487826a888b6a61d9b21854757e3ae67
Halvard Barstad <halvardbarstad@Halvards-MacBook-Pro.local>
2019-03-27 12:33:25 +0100

    Made some updates to menu

--- src/main/java/inf112/skeleton/app/MenuScreen.java
+++ src/main/java/inf112/skeleton/app/MenuScreen.java
@@ -1,7 +1,6 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -10,7 +9,11 @@ public class MenuScreen
     private SpriteBatch batch;
     private boolean run;
     private Sprite startGame;
+    private Sprite testsButton;
+    private Sprite createNewMap;
     private SpriteContainer spriteContainer;
+    private boolean runTests;
+    private Sprite drawSpritesBtn;
 
     public MenuScreen(SpriteBatch batch)
     {
@@ -18,20 +21,38 @@ public class MenuScreen
         this.run = true;
         this.spriteContainer = new SpriteContainer(batch);
 
-        this.startGame = spriteContainer.setSprite("./assets/cards/dontpress.png");
+        this.startGame = spriteContainer.setSprite("./assets/cards/startGameButton.png");
         this.startGame.setPosition(200, 220);
+        this.testsButton = spriteContainer.setSprite("./assets/cards/ManualTestsButton.png");
+        this.testsButton.setPosition(400,220);
+        this.createNewMap = spriteContainer.setSprite("./assets/cards/CreateNewMapButton.png");
+        this.createNewMap.setPosition(600,220);
+        this.runTests = false;
 
 
     }
     public void startMenu(){
 
-        batch.begin();
-        startGame.draw(batch);
-        batch.end();
+
 
 
     }
 
+    public void render(){
+
+
+        batch.begin();
+        startGame.draw(batch);
+        testsButton.draw(batch);
+        createNewMap.draw(batch);
+        batch.end();
+
+    }
+
+    public boolean runTests(){
+        return this.runTests;
+
+    }
 
     public boolean runMenu()
     {
@@ -47,6 +68,31 @@ public class MenuScreen
         return spriteContainer.isInsideSprite(screenX,screenY,startGame);
 
     }
+    public boolean clickTestStart(int screenX, int screenY){
+        if(runTests){
+            this.runTests=false;
+
+        } else {
+            this.runTests=true;
+        }
+        return spriteContainer.isInsideSprite(screenX,screenY,testsButton);
+
+    }
+
+    public void testMenu(){
+        batch.begin();
+        spriteContainer.drawTextBox("Which manual test do you want to run?",500);
+        this.drawSpritesBtn = spriteContainer.setSprite("./assets/cards/drawAllSpritesBtn.png");
+        this.drawSpritesBtn.setPosition(500,50);
+        this.drawSpritesBtn.draw(batch);
+        testsButton.draw(batch);
+
+
+
+        batch.end();
+}
+
+
 
 
 
