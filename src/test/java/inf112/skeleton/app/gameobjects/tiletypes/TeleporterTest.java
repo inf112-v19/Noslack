7f555e42c01df71e75b16b6e2ce08725250bc2f1
= <=>
2019-03-28 15:43:57 +0100

    Teleporter coupling test

--- src/test/java/inf112/skeleton/app/gameobjects/tiletypes/TeleporterTest.java
+++ src/test/java/inf112/skeleton/app/gameobjects/tiletypes/TeleporterTest.java
@@ -2,6 +2,7 @@ package inf112.skeleton.app.gameobjects.tiletypes;
 
 import inf112.skeleton.app.TileGrid;
 import inf112.skeleton.app.gameobjects.Coordinate;
+import inf112.skeleton.app.gameobjects.GameObjectType;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,10 +16,19 @@ public class TeleporterTest {
     }
 
     @Test
+    public void teleporterCoupling() {
+        TileGrid tileGrid = new TileGrid("teleporterTestMap.txt");
+        Teleporter teleporter1 = (Teleporter) tileGrid.getTile(1,1).getGameObject(GameObjectType.TELEPORTER);
+        Teleporter teleporter2 = (Teleporter) tileGrid.getTile(0,1).getGameObject(GameObjectType.TELEPORTER);
+        assertEquals(teleporter2.getPosition(), teleporter1.getTeleportLocation());
+        assertEquals(teleporter1.getPosition(), teleporter2.getTeleportLocation());
+    }
+
+    @Test
     public void teleportationCheck() {
         TileGrid tileGrid = new TileGrid("teleporterTestMap.txt");
         tileGrid.activateTiles();
-        assertEquals(new Coordinate(1,2), tileGrid.getPlayerPosition(0));
+        assertEquals(new Coordinate(0,1), tileGrid.getPlayerPosition(0));
     }
 
 }
