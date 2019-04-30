055dd304a6d295295a317084b804a02827f9b87d
Halvard Barstad <halvardbarstad@pc-104-54.eduroam.uib.no>
2019-04-30 15:38:11 +0200

    Win & loose screen start

--- src/main/java/inf112/skeleton/app/MenuScreen.java
+++ src/main/java/inf112/skeleton/app/MenuScreen.java
@@ -17,6 +17,12 @@ public class MenuScreen
     private Sprite drawSpritesBtn;
     private Sprite background;
 
+    private Sprite youWin;
+    private Sprite youLose;
+    private boolean runWin;
+    private boolean runLoose;
+    private Sprite mainMenuBtn;
+
     public MenuScreen(SpriteBatch batch)
     {
         this.batch=batch;
@@ -34,14 +40,17 @@ public class MenuScreen
         this.hostGame.setPosition(375,95);
         this.runTests = false;
 
+        this.youLose = spriteContainer.setSprite("./assets/menuScreen/youLose.png");
+        this.youLose.setPosition(225,400);
+        this.youWin = spriteContainer.setSprite("./assets/menuScreen/youWon.png");
+        this.youWin.setPosition(225,400);
+        this.mainMenuBtn = spriteContainer.setSprite("./assets/menuScreen/mainMenuBtn.png");
+        this.mainMenuBtn.setPosition(325, 100);
+        this.runWin = true;
+        this.runLoose = true;
 
     }
-    public void startMenu(){
 
-
-
-
-    }
 
     public void render(){
 
@@ -84,4 +93,27 @@ public class MenuScreen
         testsButton.draw(batch);
         batch.end();
     }
+
+    public void gameFinishMenu(){
+
+        batch.begin();
+        background.draw(batch);
+        mainMenuBtn.draw(batch);
+        if(runWin){
+            youWin.draw(batch);
+        }
+        else if(runLoose){
+            youLose.draw(batch);
+        }
+        batch.end();
+    }
+
+    public boolean win(){
+        runLoose = false;
+        return runWin;
+    }
+
+    public boolean loose(){
+        runWin = false;
+        return runLoose;}
 }
--- src/main/java/inf112/skeleton/app/SomeClass.java
+++
@@ -1,5 +0,0 @@
-package inf112.skeleton.app;
-
-public class SomeClass {
-    public String text;
-}
